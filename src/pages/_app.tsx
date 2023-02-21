84f7a7d24c855d13c2f7909843d67ec2c363f767
LIONMAIL\brianjiang <brianjiang@liontravel.com>
2023-02-21 15:36:26 +0800

    some comment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { useCreateStore, Provider } from "../lib/store";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   const createStore = useCreateStore(pageProps.initialZustandState);
   return (
     <Provider createStore={createStore}>
@@ -8,3 +9,9 @@ export default function App({ Component, pageProps }) {
     </Provider>
   );
 }
+
+// import "@/styles/globals.css";
+// import type { AppProps } from "next/app";
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />;
+// }
